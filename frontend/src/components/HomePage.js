e43c2b84db7d4949972b9f1e310c96549ff6fe2f
louis <louisryan@Louiss-MacBook-Air.local>
2021-11-21 18:53:46 +0100

    post request landing at server endpoint from front

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -5,11 +5,33 @@ import BreakLease from "./BreakLease";
 const HomePage = (props) => {
 
 
+    const handleButtonPressed = () => {
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                votes_to_skip: 700,
+                guest_can_pause: true,
+            }),
+        };
+        fetch("/api/create-room", requestOptions)
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    };
+
+
     return (
         <div>
             This is the home page !!!
-        </div>
 
+            <div>
+                <button
+                    onClick={() => handleButtonPressed()}
+                >
+                    PRESS ME
+                </button>
+            </div>
+        </div>
     );
 };
 
